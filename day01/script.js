3ee8db1e3b4ede82f19b300498df278c481fb043
조정훈 <hoon7812@hanmail.net>
2026-09-16 08:28:32 +0900

    1 commit

--- day01/script.js
+++ day01/script.js
@@ -1,7 +1,9 @@
-const fruits = ['사과', '바나나', '복숭아', '포도'];
+const fruits = ['사과', '바나나', '복숭아', '포도', '수박'];
 
 console.log(fruits);
 console.log(fruits[0]);
 console.log(fruits[1]);
 console.log(fruits[2]);
 console.log(fruits[3]);
+console.log(fruits[4]);
+console.log(fruits.length);
