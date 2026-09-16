a7c6a814cce966b7555ec6655701a5ddb41805c2
조정훈 <hoon7812@hanmail.net>
2026-09-16 17:58:06 +0900

    commit 3

--- day01/script.js
+++ day01/script.js
@@ -25,6 +25,7 @@ const result = fruits.map(function (fruit) {
     return fruit + ' 🍎';
 });
 
+console.log(fruits);
 
 
 
@@ -52,10 +53,72 @@ const result3 = numbers.map(number => number * 2);
 
 
 
-
-
-console.log(fruits);
 console.log(result);
 console.log(result1);
 console.log(result2);
 console.log(result3);
+
+
+
+
+
+
+//filter 스터디
+
+//map = 바꾼다, 변환
+//filter = 고른다, 선택
+
+const result4 = numbers.filter(number => number >= 3);
+
+console.log(result4);
+
+
+
+
+const users = [
+    { name: '철수', age: 25 },
+    { name: '영희', age: 35 },
+    { name: '민수', age: 42 }
+];
+
+const result5 = users.map(user => user.name);
+
+const result6 = users.filter(user => user.age >= 40);
+
+const result7 = users
+    .filter(user => user.age >= 40)
+    .map(user => user.name);
+
+
+console.log(result5);
+console.log(result6);
+console.log(result7);
+
+
+const numbers2 = [10, 23, 35, 42, 57, 68];
+const result8 = numbers2.filter(number => number >= 50);
+console.log(result8);
+
+
+
+
+const users2 = [
+    { name: '철수', age: 25 },
+    { name: '영희', age: 35 },
+    { name: '민수', age: 42 },
+    { name: '지수', age: 51 }
+];
+
+const result9 = users2
+    .filter(user => user.age >= 40)
+    .map(user => user.name);
+
+console.log(result9);
+
+
+// 이런 걸 메서드 체이닝(method chaining)
+// users2
+//     .filter(...)
+//     .map(...)
+
+// 배열을 반환하는 메서드 뒤에 또 다른 메서드를 붙이는 방식
